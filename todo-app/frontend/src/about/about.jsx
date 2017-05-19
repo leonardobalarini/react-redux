781be4fd79ef03bb74d7dcf4f88bcf72b3fe4197
Leonardo Balarini <leonardobalarini@gmail.com>
2017-05-18 22:20:25 -0300

    Aplicaçao To-do (incompleta)

---
+++ todo-app/frontend/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props =>(
+    <div>
+        <PageHeader name='Sobre' small="Nós"></PageHeader>
+
+        <h2>Nossa Historia</h2>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti rerum, unde nesciunt minus repellendus? Maiores, suscipit placeat facere sit aspernatur molestias magni itaque, deleniti eligendi quam voluptatum vero, vitae enim.
+        <h2>Missão</h2>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti rerum, unde nesciunt minus repellendus? Maiores, suscipit placeat facere sit aspernatur molestias magni itaque, deleniti eligendi quam voluptatum vero, vitae enim.
+        <h2>Imprensa</h2>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti rerum, unde nesciunt minus repellendus? Maiores, suscipit placeat facere sit aspernatur molestias magni itaque, deleniti eligendi quam voluptatum vero, vitae enim.
+    </div>
+)
